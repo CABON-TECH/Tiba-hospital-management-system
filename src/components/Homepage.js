b91b158748165a0cbadb996c556e919905e6c5eb
CABON-TECH <kishushubonifacembogho@gmail.com>
2023-03-03 17:12:09 +0300

    updated

---
+++ src/components/Homepage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>homepage</div>
+  )
+}
+
+export default HomePage;
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+import { RouterProvider } from 'react-router';
+import router from './router';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
