492ba105d457abc826a185286ce367f786f803cd
CABON-TECH <kishushubonifacembogho@gmail.com>
2023-03-06 16:50:11 +0300

    styling the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Navbar } from './views/Navbar'
 function App() {
 
   return (
     <div className="hello">
-      app
+      <Navbar />
     </div>
   )
 }
---
+++ src/views/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbarlogo">
+          <h1>Logo</h1>
+          <h1>login</h1>
+        </div>
+        <hr/>
+        <div className="navbarlinks">
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Services</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+
+      </nav>
+    </>
+  )
+}
